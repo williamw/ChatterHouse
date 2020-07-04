73d33306eb57e82db2aa58b0497e1d7b75ab31a9
Bill Welense <welense@gmail.com>
2020-07-04 15:20:18 -0500

    Added silence / stop listening feature

--- ChatterHouse/AppDelegate.swift
+++ ChatterHouse/AppDelegate.swift
@@ -15,6 +15,7 @@ import MultipeerKit
 class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     
     var sharedBroadcastStatus: Bool!
+    var sharedListeningStatus: Bool!
     
     var popover: NSPopover!
     var statusBarItem: NSStatusItem!
@@ -33,26 +34,26 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
         return t
     }()
     
-    private lazy var dataSource: MultipeerDataSource = {
-        MultipeerDataSource(transceiver: transceiver)
-    }()
-    
-    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView().environmentObject(dataSource)
-        
+        // Keep track of whether or not we're broadcasting
         sharedBroadcastStatus = false
         
+        // Keep track of whether or not we're listening
+        sharedListeningStatus = true
+        
+        // Start listening for broadcasts
         transceiver.resume()
         
+        // Setup the menubar icon
         self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
         if let button = self.statusBarItem.button {
             button.image = NSImage(named: "Icon-Off")
             button.action = #selector(statusBarButtonClicked(sender:))
             button.sendAction(on: [.leftMouseUp, .rightMouseUp])
-            // button.action = #selector(togglePopover(_:))
         }
+        
+        // Create the SwiftUI view that provides the Preferences popover
+        let contentView = ContentView()
         
         let popover = NSPopover()
         popover.contentSize = NSSize(width: 400, height: 400)
@@ -60,56 +61,84 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
         popover.contentViewController = NSHostingController(rootView: contentView)
         self.popover = popover
         
-        // Global hotkey
-        guard let keyCombo = KeyCombo(doubledCocoaModifiers: .control) else { return }
-        
-        // guard let keyCombo = KeyCombo(key: .b, cocoaModifiers: [.command, .control]) else { return }
-        let hotKey = HotKey(identifier: "CommandControlB",
-                            keyCombo: keyCombo,
-                            target: self,
-                            action: #selector(AppDelegate.toggleBroadcastStatus))
-        hotKey.register()
-        
-        // Close the pop-over when it loses focus
+        // Close the Preferences popover when it loses focus
         eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
             if let strongSelf = self, strongSelf.popover.isShown {
                 strongSelf.statusBarItem.menu = nil
                 strongSelf.closePopover(sender: event)
             }
         }
-    }
-    
-    @objc func toggleBroadcastStatus() {
-        if let button = self.statusBarItem.button {
-            if sharedBroadcastStatus {
-                sharedBroadcastStatus = false
-                button.image = NSImage(named: "Icon-Off")
-            } else {
-                let payload = AudioPayload(message: "\(config.peerName) started broadcasting")
-                
-                sharedBroadcastStatus = true
-                button.image = NSImage(named: "Icon-On")
-                
-                transceiver.broadcast(payload)
-            }
-        } else { return }
+        
+        // Setup the global hotkey
+        guard let keyCombo = KeyCombo(doubledCocoaModifiers: .control) else { return }
+        // guard let keyCombo = KeyCombo(key: .b, cocoaModifiers: [.command, .control]) else { return }
+        let hotKey = HotKey(identifier: "CommandControlB",
+                            keyCombo: keyCombo,
+                            target: self,
+                            action: #selector(AppDelegate.toggleBroadcastStatus))
+        hotKey.register()
     }
     
     // Detect if left or right-click on icon
     @objc func statusBarButtonClicked(sender: NSStatusBarButton) {
         let event = NSApp.currentEvent!
-
+        
         if (event.type ==  NSEvent.EventType.rightMouseUp) ||
             (event.modifierFlags.contains(.control)) {
-            constructMenu()
+            // Right-click or ctrl+click
+            presentMenu()
             sender.performClick(nil)
         } else {
+            // Left-click
             toggleBroadcastStatus()
-            //togglePopover(sender)
         }
     }
     
-    // Show/hide main app popover
+    // Start / stop broadcasting
+    @objc func toggleBroadcastStatus() {
+        if sharedBroadcastStatus {
+            stopBroadcasting()
+        } else {
+            startBroadcasting()
+        }
+    }
+    
+    func stopBroadcasting() {
+        if let button = self.statusBarItem.button {
+            sharedBroadcastStatus = false
+            button.image = NSImage(named: "Icon-Off")
+        }
+    }
+    
+    func startBroadcasting() {
+        if let button = self.statusBarItem.button {
+            let payload = AudioPayload(message: "\(config.peerName) started broadcasting")
+            
+            sharedBroadcastStatus = true
+            button.image = NSImage(named: "Icon-On")
+            
+            transceiver.broadcast(payload)
+        }
+    }
+    
+    // Start / stop listening
+    @objc func toggleListeningStatus() {
+        if let button = self.statusBarItem.button {
+            if sharedListeningStatus {
+                sharedListeningStatus = false
+                stopBroadcasting()
+                transceiver.stop()
+                button.image = NSImage(named: "Icon-Silenced")
+            } else {
+                sharedListeningStatus = true
+                transceiver.resume()
+                button.image = NSImage(named: "Icon-Off")
+
+            }
+        }
+    }
+    
+    // Show/hide Preferences popover
     @objc func togglePopover(_ sender: Any?) {
         if popover.isShown {
             closePopover(sender: sender)
@@ -132,8 +161,9 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
         eventMonitor?.stop()
     }
     
-    //⌃
-    func constructMenu() {
+    // Setup the icon's right-click menu
+    // Ctrl character: ⌃
+    func presentMenu() {
         var startStop: String!
         
         if sharedBroadcastStatus {
@@ -144,12 +174,24 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
         
         let menu = NSMenu()
         menu.addItem(NSMenuItem(title: "\(startStop!) Broadcasting", action: #selector(toggleBroadcastStatus), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Silence", action: #selector(toggleListeningStatus), keyEquivalent: ""))
         menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Quit ChatterHouse", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        menu.addItem(NSMenuItem(title: "Preferences...", action: #selector(togglePopover(_:)), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Quit ChatterHouse", action: #selector(NSApplication.terminate(_:)), keyEquivalent: ""))
+        
+        if let listeningItem = menu.item(withTitle: "Silence") {
+            if sharedListeningStatus == true {
+                listeningItem.state = NSControl.StateValue.off
+            } else {
+                listeningItem.state = NSControl.StateValue.on
+            }
+        }
+        
         statusBarItem.menu = menu
         statusBarItem.menu?.delegate = self
     }
     
+    // Reset left/right-click detection when menu closes
     @objc func menuDidClose(_ menu: NSMenu) {
         statusBarItem.menu = nil // remove menu so menu icon works as before
     }
